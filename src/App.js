db5e5571b487ed5ebe6192e98be4f7a95036a97a
ajaybasnyat <ajaybasnyat@gmail.com>
2021-03-09 22:40:22 -0600

    app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react"
 
-function App() {
+const App = () => {
+  const CLIENT_ID = "02c458fbde8b405ca33a9ce698c989e3";
+  const CLIENT_SECRET = "c29d45caaf114e9b9fcad204dd174aea";
+  var Spotify = require('spotify-web-api-js');
+  var s = new Spotify();
+  var spotifyApi = new SpotifyWebApi();
+  const result = await fetch('https://accounts.spotify.com/api/token'), {
+    method: 'POST',
+    headers: {
+      'Content-Type' : 'application/x-www-form-urlencoded',
+      'Authorization' : 'Basic ' + btoa(CLIENT_ID + ':' + CLIENT_SECRET)
+    },
+    body: 'grant_type=client_credentials'
+  });
+  const data = await result.json();
+  token = data.access_token
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello </h1>
     </div>
-  );
+    console.log(data);
+  )
 }
 
 export default App;
