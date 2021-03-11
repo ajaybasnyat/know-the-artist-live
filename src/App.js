e59d860f818179b7ce6f0524ad1f02175e14e454
ajaybasnyat <ajaybasnyat@gmail.com>
2021-03-10 23:54:57 -0600

    com

--- src/App.js
+++ src/App.js
@@ -1,30 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
-import React from "react"
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+
+const APIController = (function () {
+  const _getToken = async () => {
+    const CLIENT_ID = "02c458fbde8b405ca33a9ce698c989e3";
+    const CLIENT_SECRET = "c29d45caaf114e9b9fcad204dd174aea";
+
+    const result = await fetch("https://accounts.spotify.com/api/token", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        Authorization: "Basic " + btoa(CLIENT_ID + ":" + CLIENT_SECRET),
+      },
+      body: "grant_type=client_credentials",
+    });
+    const data = await result.json();
+    return data.access_token;
+  };
+
+  return {
+    getToken() {
+      return _getToken();
+    }
+  }
+})();
 
 const App = () => {
-  const CLIENT_ID = "02c458fbde8b405ca33a9ce698c989e3";
-  const CLIENT_SECRET = "c29d45caaf114e9b9fcad204dd174aea";
-  var Spotify = require('spotify-web-api-js');
-  var s = new Spotify();
-  var spotifyApi = new SpotifyWebApi();
-  const result = await fetch('https://accounts.spotify.com/api/token'), {
-    method: 'POST',
-    headers: {
-      'Content-Type' : 'application/x-www-form-urlencoded',
-      'Authorization' : 'Basic ' + btoa(CLIENT_ID + ':' + CLIENT_SECRET)
-    },
-    body: 'grant_type=client_credentials'
-  });
-  const data = await result.json();
-  token = data.access_token
-
+  var token = APIController.getToken()
+  console.log(token)
   return (
     <div className="App">
       <h1>Hello </h1>
+      <form className="search-form">
+        <input className="search-bar" type="text"/>
+        <button className="search-button" type="submit">Search</button>
+      </form>
     </div>
-    console.log(data);
-  )
-}
+  );
+};
 
 export default App;
