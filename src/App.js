4e309d0b1389b12abf86ed5d188174ce3d93dca2
ajaybasnyat <ajaybasnyat@gmail.com>
2021-03-13 22:55:30 -0600

    search artist implementation

--- src/App.js
+++ src/App.js
@@ -1,57 +1,80 @@
 import "./App.css";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import SpotifyWebApi from "spotify-web-api-js";
 
 const App = () => {
-  const [token, setToken] = useState('');
+  const [token, setToken] = useState("");
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("");
+  const initialRender = useRef(true);
+  
   useEffect(() => {
-    const APIController = (function () {
-      const _getToken = async () => {
-        const CLIENT_ID = "02c458fbde8b405ca33a9ce698c989e3";
-        const CLIENT_SECRET = "c29d45caaf114e9b9fcad204dd174aea";
+    getToken();
+  },[]);
 
-        const result = await fetch("https://accounts.spotify.com/api/token", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
-            Authorization: "Basic " + btoa(CLIENT_ID + ":" + CLIENT_SECRET),
-          },
-          body: "grant_type=client_credentials",
-        });
-        const data = await result.json();
-        console.log(data)
-        setToken(data.access_token)
-        return data.access_token;
-      };
+  useEffect(() => {
+    console.log(initialRender);
+    if(initialRender.current) {
+      initialRender.current = false;
+    }
+    else {
+      searchArtist();
+    }
+  }, [query]);
 
-      return {
-        getToken() {
-          return _getToken();
-        },
-      };
-    })();
+  const getToken = async () => {
 
-    APIController.getToken()
+    const CLIENT_ID = "02c458fbde8b405ca33a9ce698c989e3";
+    const CLIENT_SECRET = "c29d45caaf114e9b9fcad204dd174aea";
 
-  });
+    const result = await fetch("https://accounts.spotify.com/api/token", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        Authorization: "Basic " + btoa(CLIENT_ID + ":" + CLIENT_SECRET),
+      },
+      body: "grant_type=client_credentials",
+    });
+    const data = await result.json();
+    setToken(data.access_token);
+    return data.access_token;
+  };
 
-  console.log(token);
+
   var Spotify = require("spotify-web-api-js");
   var spotifyApi = new SpotifyWebApi();
   spotifyApi.setAccessToken(token);
-  spotifyApi.getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE', function (err, data) {
-    if (err) console.error(err);
-    else console.log('Artist albums', data);
-  });
+  
+  const searchArtist = async () => {
+    spotifyApi.searchArtists(query).then(
+      function (data) {
+        console.log('res: ', data);
+      },
+      function (err) {
+        console.error(err);
+      }
+    )
+  }
+
+  const updateSearch = e => {
+    setSearch(e.target.value)
+    console.log(search)
+  }
+
+  const getSearch = e => {
+    
+    e.preventDefault();
+    setQuery(search)
+  }
+
   return (
     <div className="App">
       <h1>Hello </h1>
-      <form className="search-form">
-        <input className="search-bar" type="text" />
+      <form className="search-form" onSubmit={getSearch}>
+        <input className="search-bar" type="text" value={search} onChange={updateSearch}/>
         <button className="search-button" type="submit">
           Search
         </button>
-        <p>{token}</p>
       </form>
     </div>
   );
